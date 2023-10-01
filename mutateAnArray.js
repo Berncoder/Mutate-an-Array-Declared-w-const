8978d64a2c6c03bcb88a895939542ab548e585fc
Bernadette <bernadettemetran35@gmail.com>
2023-09-30 22:27:19 -0700

    first commit

---
+++ mutateAnArray.js
@@ -0,0 +1,11 @@
+//An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
+
+
+const s = [5, 7, 2];
+function editInPlace() {
+    s[0] = 2;
+    s[1] = 5;
+    s[3] = 7;
+}
+console.log(s);
+editInPlace();
